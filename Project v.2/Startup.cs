9199cfb671cb250df015eba6d91664d9e1b45cfd
Elkady98 <mostafaelkady.ce@gmail.com>
2019-12-09 17:14:11 +0200

    v1.0

---
+++ Project v.2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_v._2.Startup))]
+namespace Project_v._2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
